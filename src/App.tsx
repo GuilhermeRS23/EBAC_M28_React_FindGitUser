d99537cffc1b052f5838889fdb0c00d65de6671c
GuilhermeRS <guilhermemuralha2016@gmail.com>
2025-05-26 11:04:29 -0300

    Base para reiniciar o prjeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from 'react';
-import Form from './components/Form';
-import Perfil from './components/Perfil';
-import './App.css'
-import ReposList from './components/ReposList';
-
 const App = () => {
-  const [visible, setVisible] = useState(true);
-
   return (
     <>
-      <Perfil nome="Guilherme Rosa" username='guilhermers23' />
-      <ReposList username="guilhermers23" />
-      {visible && <Form />}
-      <button onClick={() => setVisible(!visible)}>{!visible ? "Mostrar" : "Ocultar"} Form</button>
+      <h1>Hello World!</h1>
     </>
   )
 }
 
-export default App; 
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css';
-import App from './App.tsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './globalStyles.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
